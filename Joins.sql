83b64750735164364c959b03172f157111374d12
mylukishore0731 <kishoredmylu0731@gmail.com>
2025-07-24 17:25:27 +0530

    Update Joins.sql

--- Joins.sql
+++ Joins.sql
@@ -1,24 +1,39 @@
 -- Inner Join
-SELECT Flights.FlightNumber, Flights.DepartureAirport, Airports.AirportName
+SELECT 
+  Flights.FlightNumber, 
+  Flights.DepartureAirport, 
+  Airports.AirportName
 FROM Flights
-INNER JOIN Airports ON Flights.DepartureAirport = Airports.AirportCode;
+  INNER JOIN Airports ON Flights.DepartureAirport = Airports.AirportCode;
 
 -- Left Join
-SELECT Flights.FlightNumber, Flights.DepartureAirport, Airports.AirportName
+SELECT 
+  Flights.FlightNumber, 
+  Flights.DepartureAirport, 
+  Airports.AirportName
 FROM Flights
-LEFT JOIN Airports ON Flights.DepartureAirport = Airports.AirportCode;
+  LEFT JOIN Airports ON Flights.DepartureAirport = Airports.AirportCode;
 
 -- Right Join
-SELECT Flights.FlightNumber, Flights.DepartureAirport, Airports.AirportName
+SELECT 
+  Flights.FlightNumber, 
+  Flights.DepartureAirport, 
+  Airports.AirportName
 FROM Flights
-RIGHT JOIN Airports ON Flights.DepartureAirport = Airports.AirportCode;
+  RIGHT JOIN Airports ON Flights.DepartureAirport = Airports.AirportCode;
 
 -- Full Outer Join
-SELECT Flights.FlightNumber, Flights.DepartureAirport, Airports.AirportName
+SELECT 
+  Flights.FlightNumber, 
+  Flights.DepartureAirport, 
+  Airports.AirportName
 FROM Flights
-FULL OUTER JOIN Airports ON Flights.DepartureAirport = Airports.AirportCode;
+  FULL OUTER JOIN Airports ON Flights.DepartureAirport = Airports.AirportCode;
 
 -- Cross Join
-SELECT Flights.FlightNumber, Flights.DepartureAirport, Airports.AirportName
+SELECT 
+  Flights.FlightNumber, 
+  Flights.DepartureAirport, 
+  Airports.AirportName
 FROM Flights
-CROSS JOIN Airports;
+  CROSS JOIN Airports;
